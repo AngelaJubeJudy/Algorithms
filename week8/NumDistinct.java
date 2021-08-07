ca08a5cc7e54a9e78c2d81c8d26c591fefbf20ca
Diqing Zhu <angelajubejudy@yahoo.com.hk>
2021-08-07 22:27:14 +0800

    115.不同的子序列
    【LeetCode】https://leetcode-cn.com/problems/distinct-subsequences/

---
+++ week8/NumDistinct.java
@@ -0,0 +1,21 @@
+class NumDistinct {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        s = ' ' + s;
+        t = ' ' + t;
+        int[][] f = new int[n+1][m+1];
+        for(int i = 0; i <= n; i++){
+            f[i][0] = 1;  // 空串是任何字符串的子串
+        }
+
+        // 动规
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                f[i][j] = f[i-1][j];
+                if(s.charAt(i) == t.charAt(j)) f[i][j] += f[i-1][j-1];
+            }
+        }
+        return f[n][m];
+    }
+}
