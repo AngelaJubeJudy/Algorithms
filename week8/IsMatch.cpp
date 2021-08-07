740c7051964780d223a5c5bc113728a489fec4d8
Diqing Zhu <angelajubejudy@yahoo.com.hk>
2021-08-08 00:25:22 +0800

    10.正则表达式匹配
    https://leetcode-cn.com/problems/regular-expression-matching/

---
+++ week8/IsMatch.cpp
@@ -0,0 +1,33 @@
+class IsMatch {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
+        s = " " + s;
+        p = " " + p;
+        vector<vector<bool>> f(n+1, vector<bool>(m+1, false));
+        f[0][0] = true;
+
+        // Init
+        for(int i = 2; i <= m && p[i] == '*'; i+=2){
+            f[0][i] = true;
+        }
+
+        // DP
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(p[j] == '.'){
+                    f[i][j] = f[i-1][j-1];
+                }else if(p[j] == '*'){
+                    f[i][j] = f[i][j-2];  // 不要*
+                    if(p[j-1] == '.' || s[i] == p[j-1]){
+                        f[i][j] = f[i][j] || f[i-1][j];
+                    }
+                }else{  // 字符
+                    f[i][j] = f[i-1][j-1] && s[i] == p[j];
+                }
+            }
+        }
+        return f[n][m];
+    }
+};
